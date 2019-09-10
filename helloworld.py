811ae79ba51c63205bdb6590174e9bd90ede9644
adamzhang <adamjyz@umich.edu>
2019-09-09 21:36:42 -0400

    Pushing my final code

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,10 @@
+from random import randint
 print("Adam Zhang")
+
+a = randint(1, 1000)
+b = randint(1, 1000)
+sum = a + b
+print (a)
+print (b)
+print ("Sum = " + str(sum))
+print ("Average = " + str( sum / 2.0 ))
